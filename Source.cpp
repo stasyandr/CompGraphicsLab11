c60bb4498097712b2f9bc55a36ded63e9764e56b
dkicenko <61902129+dkicenko@users.noreply.github.com>
2021-12-02 00:34:46 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,155 @@
+#include <SFML/OpenGL.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+
+void SetIcon(sf::Window& wnd);
+void Init();
+void Draw();
+
+GLfloat move_z = 0;
+GLfloat move_x = 0;
+GLfloat move_y = 0;
+
+int main() {
+	// Создаём окно
+	sf::Window window(sf::VideoMode(600, 600), "My OpenGL window", sf::Style::Default, sf::ContextSettings(32));
+	// Ставим иконку (окна с дефолтной картинкой это некрасиво)
+	SetIcon(window);
+	// Включаем вертикальную синхронизацию (синхронизация частоты отрисовки с частотой кадров монитора, что бы картинка не фризила, делать это не обязательно)
+	window.setVerticalSyncEnabled(true);
+
+	// Активируем окно
+	window.setActive(true);
+
+	// Инициализация
+	Init();
+
+	// Главный цикл окна
+	while (window.isOpen()) {
+		sf::Event event;
+		// Цикл обработки событий окна, тут можно ловить нажатия клавиш, движения мышки и всякое такое
+		while (window.pollEvent(event)) {
+			switch (event.key.code)
+			{
+			case sf::Keyboard::Up:
+				move_x -= 0.05;
+				break;
+			case sf::Keyboard::Down:
+				move_x += 0.05;
+				break;
+			case sf::Keyboard::Right:
+				move_z -= 0.05;
+				break;
+			case sf::Keyboard::Left:
+				move_z += 0.05;
+				break;
+			case sf::Keyboard::W:
+				move_y += 0.05;
+				break;
+			case sf::Keyboard::S:
+				move_y -= 0.05;
+				break;
+			default:
+				break;
+			}
+			if (event.type == sf::Event::Closed) {
+				// Окно закрыто, выходим из цикла
+				window.close();
+			}
+			else if (event.type == sf::Event::Resized) {
+				// Изменён размер окна, надо поменять и размер области Opengl отрисовки
+				glViewport(0, 0, event.size.width, event.size.height);
+			}
+		}
+
+		// Очистка буферов
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Рисуем сцену
+		Draw();
+
+		// Отрисовывает кадр - меняет передний и задний буфер местами
+		window.display();
+	}
+
+	// — тут надо не забыть очистить все выделенные ресурсы —
+
+	return 0;
+}
+
+// В момент инициализации разумно произвести загрузку текстур, моделей и других вещей
+void Init() {
+	// Очистка буфера тёмно жёлтым цветом
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+// Функция непосредственно отрисовки сцены
+void Draw() {
+
+	// Устанавливаем единичную матрицу преобразования
+	//glLoadIdentity();
+	glEnable(GL_DEPTH_TEST);
+
+
+	// Устанавливаем единичную матрицу преобразования
+	glLoadIdentity();
+
+	glTranslatef(move_x, 0.0, 0.0);
+	glTranslatef(0.0, move_y, 0.0);
+	glTranslatef(0.0 , 0.0, -move_z);
+
+	glRotated(-90, 1.0, 0.0, 0.0);
+	glRotated(-16, 0.0, 1.0, 0.0);
+
+	//front triangle
+	glBegin(GL_TRIANGLES);
+	// Front
+	glColor3f(1.0f, 1.0f, 1.0f); glVertex3f(0.0f, 0.5f, 0.0f);//w
+	glColor4f(1.0f, 0.0f, 0.0f, 1.0f); glVertex3f(0.5f, -0.5f, 0.5f);//r
+	glColor4f(0.0f, 0.0f, 1.0f, 1.0f); glVertex3f(-0.5f, -0.5f, 0.5f);//b
+	glEnd();
+
+	glBegin(GL_TRIANGLES);
+	// Right
+	glColor3f(1.0f, 1.0f, 1.0f); glVertex3f(0.0f, 0.5f, 0.0f);//w
+	glColor4f(1.0f, 0.0f, 0.0f, 1.0f); glVertex3f(0.5f, -0.5f, 0.5f);//r
+	glColor4f(0.0f, 1.0f, 0.0f, 1.0f); glVertex3f(0.0f, -0.5f, -0.5f);//g
+	
+	glEnd();
+
+	glBegin(GL_TRIANGLES);
+	// Back
+	glColor3f(1.0f, 1.0f, 1.0f); glVertex3f(0.0f, 0.5f, 0.0f);//w
+	glColor4f(0.0f, 0.0f, 1.0f, 1.0f); glVertex3f(-0.5f, -0.5f, 0.5f);//b
+	glColor4f(0.0f, 1.0f, 0.0f, 1.0f); glVertex3f(0.0f, -0.5f, -0.5f);//g
+	glEnd();
+
+	glBegin(GL_TRIANGLES);
+	// Bottom
+	glColor4f(1.0f, 0.0f, 0.0f, 1.0f); glVertex3f(0.5f, -0.5f, 0.5f);//r
+	glColor4f(0.0f, 0.0f, 1.0f, 1.0f); glVertex3f(-0.5f, -0.5f, 0.5f);//b
+	glColor4f(0.0f, 1.0f, 0.0f, 1.0f); glVertex3f(0.0f, -0.5f, -0.5f);//g
+	glEnd();
+
+	
+
+	// Отправляем набор команд отрисовываться
+	glFlush();
+
+}
+
+
+void SetIcon(sf::Window& wnd)
+{
+	sf::Image image;
+
+	
+		image.create(16, 16);
+	for (int i = 0; i < 16; ++i)
+		for (int j = 0; j < 16; ++j)
+			image.setPixel(i, j, {
+			(sf::Uint8) (i * 16), (sf::Uint8)(j * 16), 0 });
+
+	wnd.setIcon(image.getSize().x, image.getSize().y, image.getPixelsPtr());
+}
